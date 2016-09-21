1e1b56aea7684861bf67cd71d938ac1e80bc27af
Bloemkoolsaus <bloemkoolsaus.eve@gmail.com>
2016-09-21 23:24:13 +0200

    Crest Fleet

--- modules/crest/classes/console/Fleet.php
+++ modules/crest/classes/console/Fleet.php
@@ -5,41 +5,69 @@ class Fleet
 {
     function doFleet($arguments=[])
     {
-        $fleetURL = "fleets/1047511263687/";
+        \AppRoot::doCliOutput("doFleet(".implode(",",$arguments).")");
+        /** @var \crest\model\Fleet $fleet */
+        $fleet = \crest\model\Fleet::findById(array_shift($arguments));
+        if (!$fleet)
+            return null;
 
-        $character = new \crest\model\Character(1899584653);
+        $character = new \crest\model\Character($fleet->bossID);
 
         $crest = new \crest\Api();
         $crest->setToken($character->getToken());
-        $crest->get($fleetURL);
+        $crest->get("fleets/".$fleet->id."/members/");
 
         if ($crest->success())
         {
-            if (isset($crest->getResult()->members))
+            foreach ($crest->getResult()->items as $fleetMember)
             {
-                $url = str_replace($crest->baseURL, "", $crest->getResult()->members->href);
-                $crest = new \crest\Api();
-                $crest->setToken($character->getToken());
-                $crest->get($url);
-
-                if ($crest->success())
-                {
-                    foreach ($crest->getResult()->items as $fleetMember)
-                    {
-                        echo "<pre>".print_r($fleetMember, true)."</pre>";
-
-                        \MySQL::getDB()->updateinsert("mapwormholecharacterlocations", [
-                            "characterid" => $fleetMember->character->id,
-                            "solarsystemid" => $fleetMember->solarSystem->id,
-                            "shiptypeid" => $fleetMember->ship->id,
-                            "lastdate" => date("Y-m-d H:i:s")
-                        ],[
-                            "characterid" => $fleetMember->character->id
-                        ]);
-                    }
-                }
+                \MySQL::getDB()->updateinsert("mapwormholecharacterlocations", [
+                    "characterid" => $fleetMember->character->id,
+                    "solarsystemid" => $fleetMember->solarSystem->id,
+                    "shiptypeid" => $fleetMember->ship->id,
+                    "authgroupid" => $fleet->authGroupID,
+                    "lastdate" => date("Y-m-d H:i:s")
+                ],[
+                    "characterid" => $fleetMember->character->id
+                ]);
             }
-
         }
+    }
+
+    /**
+     * get fleet
+     * @param $url
+     * @param $characterID
+     * @return \crest\model\Fleet|null
+     */
+    function getFleetByURL($url, $characterID)
+    {
+        $character = new \crest\model\Character($characterID);
+
+        $crest = new \crest\Api();
+        $crest->setToken($character->getToken());
+
+        $url = str_replace($crest->baseURL, "", $url);
+        $crest->get($url);
+
+        if ($crest->success())
+        {
+            $parts = explode("/",$url);
+            $id = $parts[1];
+
+            /** @var \crest\model\Fleet $fleet */
+            $fleet = \crest\model\Fleet::findOne(["id" => $id]);
+            if (!$fleet) {
+                $fleet = new \crest\model\Fleet();
+                $fleet->id = $id;
+            }
+            $fleet->bossID = $characterID;
+            $fleet->authGroupID = \User::getUSER()->getCurrentAuthGroupID();
+            $fleet->active = 1;
+            $fleet->store();
+            return $fleet;
+        }
+
+        return null;
     }
 }
--- modules/crest/classes/view/Fleet.php
+++ modules/crest/classes/view/Fleet.php
@@ -5,6 +5,15 @@ class Fleet
 {
     function getAdd($arguments=[])
     {
+        if (\Tools::POST("fleet"))
+        {
+
+            $console = new \crest\console\Fleet();
+            $fleet = $console->getFleetByURL(\Tools::POST("fleet"), \Tools::POST("boss"));
+            if ($fleet)
+                $console->doFleet([$fleet->id]);
+
+        }
 
         $tpl = \SmartyTools::getSmarty();
         return $tpl->fetch("crest/fleet/add");
