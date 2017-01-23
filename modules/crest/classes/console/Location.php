834929356d5130a48c255ef178ec888d664e4274
Bloemkoolsaus <bloemkoolsaus.eve@gmail.com>
2017-01-23 15:29:20 +0100

    Crest check online status

--- modules/crest/classes/console/Location.php
+++ modules/crest/classes/console/Location.php
@@ -15,19 +15,23 @@ class Location
             \AppRoot::doCliOutput("Find characters");
 
             $i = 0;
-            if ($results = \MySQL::getDB()->getRows("select 1 as online, c.*
+            if ($results = \MySQL::getDB()->getRows("select 1 as online, c.*, l.lastdate as requestdate
                                                     from    characters c
                                                         inner join crest_token t on t.tokenid = c.id and t.tokentype = 'character'
                                                         inner join map_character_locations l on l.characterid = c.id
                                                     where   l.lastdate < ?
                                                 union
-                                                    select  0 as online, c.*
+                                                    select  0 as online, c.*, clog.requestdate
                                                     from    characters c
                                                         inner join crest_token t on t.tokenid = c.id and t.tokentype = 'character'
                                                         left join map_character_locations l on l.characterid = c.id
+                                                        left join (select   max(requestdate) as requestdate, url
+                                                                    from    crest_log
+                                                                    group by url
+                                                            ) clog on clog.url = concat('characters/', c.id, '/location/')
                                                     where   l.characterid is null
-                                                    and    (c.lastonline is null or c.lastonline < ?)
-                                                order by online desc, lastonline asc, updatedate desc
+                                                    and    (clog.requestdate is null or clog.requestdate < ?)
+                                                order by online desc, requestdate asc, updatedate desc
                                                 limit 100"
                         , [ date("Y-m-d H:i:s", mktime(date("H"),date("i"),date("s")-11,date("m"),date("d"),date("Y"))),
                             date("Y-m-d H:i:s", mktime(date("H"),date("i")-10,date("s"),date("m"),date("d"),date("Y")))]))
