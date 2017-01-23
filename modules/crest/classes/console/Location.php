ce748e725ffdf305521ec9cdeb77bd29be5dc145
Bloemkoolsaus <bloemkoolsaus.eve@gmail.com>
2017-01-23 13:04:34 +0100

    crest

--- modules/crest/classes/console/Location.php
+++ modules/crest/classes/console/Location.php
@@ -27,7 +27,7 @@ class Location
                 {
                     // Asynchroon uitvoeren
                     \AppRoot::doCliOutput("> [".$result["id"]."] ".$result["name"]);
-                    exec(getcwd()."/cron.php crest location character ".$result["id"]." > /dev/null &");
+                    exec("php ".getcwd()."/cron.php crest location character ".$result["id"]." > /dev/null &");
                 }
             }
 
